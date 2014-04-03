0b9f724765c621240cfdce0381bc9ef101144e90
Matt.Hill <lepermessiah57@gmail.com>
2014-04-03 11:24:24 -0500

    adding a specialized logger aware interface

--- src/AopLogger/Initializer/ErrorLogInitializer.php
+++ src/AopLogger/Initializer/ErrorLogInitializer.php
@@ -7,10 +7,10 @@ use AopLogger\Log\ErrorLoggerAwareInterface;
 use Zend\ServiceManager\InitializerInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 
-class ErrorLogInitializer implements ErrorLoggerAwareInterface{
+class ErrorLogInitializer implements InitializerInterface{
 
     public function initialize($instance, ServiceLocatorInterface $serviceLocator) {
-        if($instance instanceof LoggerAwareInterface){
+        if($instance instanceof ErrorLoggerAwareInterface){
             $instance->setLogger($serviceLocator->get('AopLoggerErrorLog'));
         }
     }
